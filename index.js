7804f77e10a907843b41e408807dc9f868f994f9
brahim <brahimgb1team@gmail.com>
2022-02-18 19:20:05 +0000

    add luxon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { start } from './modules/start.js';
 import { listButton, addButton, contactButton } from './modules/DisplaySections.js';
 import input from './modules/input.js';
 import displayBooks from './modules/DisplayBook.js';
+import { DateTime } from './node_modules/luxon/build/es6/luxon.js';
 
 const addBtn = document.querySelector('.add_book_btn');
 const listBtn = document.querySelector('.list');
@@ -28,3 +29,14 @@ addNewBtn.addEventListener('click', () => {
 contactBtn.addEventListener('click', () => {
   contactButton();
 });
+
+// toDo
+const timeDisplay = document.querySelector('.time-display');
+
+const setTime = () => {
+  const time = DateTime.now();
+  timeDisplay.textContent = '';
+  timeDisplay.textContent = `${time.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)}`;
+};
+
+setInterval(setTime, 1000);
